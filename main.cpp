e412adf03542ce8b7b51b1f3ce8b2bfa61110bca
wilfrantz Dede <wilfrantz@outlook.com>
2021-09-23 14:27:51 -0400

    Completes UpTime()

--- main.cpp
+++ main.cpp
@@ -25,28 +25,3 @@ long LinuxParser::UpTime(int pid) {
 
   return stol(line) / sysconf(_SC_CLK_TCK);
 }
-
-/*
-long LinuxParser::UpTimes(int pid) {
-  std::string line;
-  std::ifstream UpFile(LinuxParser::kProcDirectory + std::to_string(pid) +
-                       LinuxParser::kStatFilename);
-
-  if (UpFile.is_open()) {
-    while (std::getline(UpFile, line)) {
-      std::cout << "here: " << line << std::endl;
-      int pos = line.find(pid);
-      std::cout << "There: " << std::stol(line) / sysconf(_SC_CLK_TCK)<< std::endl;
-      std::istringstream upstream(line);
-      std::string data;
-      exit (0);
-      std::istringstream upstream(line);
-      // for (int i = 0; i < 52; i++) upstream >> data;
-      // std::cout << "here: " << data << std::endl;
-      exit (0);
-    }
-  }
-    // return stol(data) / sysconf(_SC_CLK_TCK);
-    return 0;
-}
-*/
