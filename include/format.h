0d5ec629d456c14afabcb9f5640623a167128c98
wilfrantz Dede <wilfrantz@outlook.com>
2021-09-28 13:41:30 -0400

    completes format(long)

--- include/format.h
+++ include/format.h
@@ -1,6 +1,7 @@
 #ifndef FORMAT_H
 #define FORMAT_H
 
+#include <sstream>
 #include <string>
 
 namespace Format {
--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,26 @@
-#include <string>
-
 #include "format.h"
 
 using std::string;
+using std::stringstream;
+using std::to_string;
 
-// TODO: Complete this helper function
+string format(int num) {
+  stringstream Ss;
+  if (num < 10) Ss << "0";
+  Ss << num;
+  return Ss.str();
+}
+
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  const long mnt = (seconds / 60) % 60;
+  const long hrs = seconds / 3600;
+  const long sec = seconds % 60;
+
+  std::stringstream stream;
+  stream << format(hrs) << ":" << format(mnt) << ":" << format(sec);
+  return stream.str();
+}
